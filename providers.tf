26f9329523343e64475f1ca95eef32826254732f
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-23 19:44:13 -0300

    Add Terraform configuration for AWS provider and VPC resource

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
